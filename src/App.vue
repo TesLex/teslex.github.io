bf777214fb8038d7bbad232d86f6409a4afb6826
expexes <devid.kohut@gmail.com>
2017-11-17 20:57:42 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-container>
+      <nav-bar></nav-bar>
+    </b-container>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import NavBar from './components/nav/NavBar.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      NavBar
+    }
+  }
+</script>
+
+<style>
+</style>
