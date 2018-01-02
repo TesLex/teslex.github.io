fa7e9ac15be714a3d8950f5c28818640e18b47f2
expexes <devid.kohut@gmail.com>
2018-01-03 01:33:19 +0200

    ~

---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="menu">
+    <div class="mid">
+      <router-link to="/portfolio">Portfolio</router-link>
+      <router-link to="/projects">Projects</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "navigation"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="portfolio">
+    <div class="card" v-for="p in portfolio">
+      <div class="title">{{ p.title }}</div>
+      <p class="content">
+        {{ p.description }}
+      </p>
+      <div class="footer">
+        <a target="_blank" :href="p.github">GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PortfolioData from '@/assets/data/portfolio'
+
+  export default {
+    name: 'Portfolio',
+    data() {
+      return {
+        portfolio: PortfolioData
+      }
+    },
+    created() {
+      //
+    }
+  }
+</script>
+
+<style scoped>
+  a {
+    color: #333;
+  }
+</style>
